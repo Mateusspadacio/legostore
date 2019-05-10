851154e348465e6ccab51c40b0192819bf4552b1
Mateusspadacio <mateusspadacio@gmail.com>
2019-05-10 19:57:46 -0300

    Update LegoStoreController.java

--- src/main/java/rc/legostore/controller/LegoStoreController.java
+++ src/main/java/rc/legostore/controller/LegoStoreController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,7 +42,8 @@ public class LegoStoreController {
 	
 	@GetMapping("/all")
 	public ResponseEntity<Collection<LegoSet>> all() {
-		Collection<LegoSet> legoSets = legoSetRepository.findAll();
+		Sort sortByThemeAsc = Sort.by("theme").ascending();
+		Collection<LegoSet> legoSets = legoSetRepository.findAll(sortByThemeAsc);
 		return ResponseEntity.ok(legoSets);
 	}
 	
